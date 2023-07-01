cbd57ade0b885affef055bf82085bc92b92dbcc4
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-07-01 16:17:20 +0200

    Add solution  for day 1 2015.

---
+++ year-2015/day-01/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main (void) {
+    char ch;
+    int floor = 0;
+    int position = 0;
+    int firstPositionAtBasement = 0;
+    bool haveFoundTheBasement = false;
+    fstream fin("my-input.txt", fstream::in);
+    while (fin >> noskipws >> ch) {
+        position++;
+        
+        switch (ch) {
+        case '(':
+        floor++;
+        break;
+        case ')':
+        floor--;
+        break;
+        default:
+        cout << "Unexpected input, terminating execution";
+        return 1;
+        }
+        
+        bool firstTimeAtBasement = !haveFoundTheBasement && floor == -1;
+        if (firstTimeAtBasement){
+            firstPositionAtBasement = position;
+            haveFoundTheBasement = true;
+        }
+    }
+    cout << "Answer Part One = " << floor << endl;
+    cout << "Answer Part Two = " << firstPositionAtBasement;
+    return 0;
+}
