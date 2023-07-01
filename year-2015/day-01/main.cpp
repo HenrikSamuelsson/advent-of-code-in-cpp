61343553c8faf46b73c49dc79b4694e0215aa217
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-07-01 16:26:56 +0200

    Auto formatted version of the previous code.

--- year-2015/day-01/main.cpp
+++ year-2015/day-01/main.cpp
@@ -3,35 +3,41 @@
 
 using namespace std;
 
-int main (void) {
+int main(void)
+{
     char ch;
     int floor = 0;
     int position = 0;
     int firstPositionAtBasement = 0;
     bool haveFoundTheBasement = false;
     fstream fin("my-input.txt", fstream::in);
-    while (fin >> noskipws >> ch) {
+    while (fin >> noskipws >> ch)
+    {
         position++;
-        
-        switch (ch) {
+
+        switch (ch)
+        {
         case '(':
-        floor++;
-        break;
+            floor++;
+            break;
         case ')':
-        floor--;
-        break;
+            floor--;
+            break;
         default:
-        cout << "Unexpected input, terminating execution";
-        return 1;
+            cout << "Unexpected input, terminating execution";
+            return 1;
         }
-        
+
         bool firstTimeAtBasement = !haveFoundTheBasement && floor == -1;
-        if (firstTimeAtBasement){
+        if (firstTimeAtBasement)
+        {
             firstPositionAtBasement = position;
             haveFoundTheBasement = true;
         }
     }
+
     cout << "Answer Part One = " << floor << endl;
     cout << "Answer Part Two = " << firstPositionAtBasement;
+    
     return 0;
 }
