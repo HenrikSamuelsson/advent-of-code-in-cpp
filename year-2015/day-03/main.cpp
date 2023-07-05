d41811e1a48324870248ce1ce9ecb312e35c195f
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-07-05 11:17:37 +0200

    Correct the indentation.

--- year-2015/day-03/main.cpp
+++ year-2015/day-03/main.cpp
@@ -4,14 +4,15 @@
 
 int main(void)
 {
-    char dir;     // Tracks the directions instructions provided by the elf
-    int x = 0;    // Tracks santa's x coordinate as he moves between houses
-    int y = 0;    // Tracks santa's y coordinate as he moves between houses
-    std::pair <int, int> houseCoordinates (0, 0);
-    std::map<std::pair<int, int>, int> houseMap;    // Tracks visited houses
+    char dir;  // Tracks the directions instructions provided by the elf
+    int x = 0; // Tracks santa's x coordinate as he moves between houses
+    int y = 0; // Tracks santa's y coordinate as he moves between houses
+    std::pair<int, int> houseCoordinates(0, 0);
+    std::map<std::pair<int, int>, int> houseMap; // Tracks visited houses
 
     std::fstream fin("my-input-year-2015-day-03.txt", std::fstream::in);
-    while (fin >> dir) {
+    while (fin >> dir)
+    {
         switch (dir)
         {
         case '^':
@@ -20,7 +21,7 @@ int main(void)
         case 'v':
             y--;
             break;
-             case '>':
+        case '>':
             x++;
             break;
         case '<':
@@ -30,18 +31,17 @@ int main(void)
             std::cout << "Unexpected input, terminating execution";
             return 1;
         }
-    
-    std::map<std::pair<int, int>, int>::iterator itr;
-    itr = houseMap.find(std::make_pair(x,y));
-    if (itr != houseMap.end())
-    {
-        itr->second++;
-    }
-    else
-    {
-        houseMap.insert(std::make_pair(std::make_pair(x, y), 1));
-    }
 
+        std::map<std::pair<int, int>, int>::iterator itr;
+        itr = houseMap.find(std::make_pair(x, y));
+        if (itr != houseMap.end())
+        {
+            itr->second++;
+        }
+        else
+        {
+            houseMap.insert(std::make_pair(std::make_pair(x, y), 1));
+        }
     }
 
     std::cout << "Answer Part One = " << houseCoordinates.first << std::endl;
