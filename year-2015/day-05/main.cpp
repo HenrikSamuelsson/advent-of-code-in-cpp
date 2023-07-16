eebc7fcd928ba7d34a6e85397f5736c45ec479c4
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-07-16 17:14:23 +0200

    Code clean up.

--- year-2015/day-05/main.cpp
+++ year-2015/day-05/main.cpp
@@ -13,13 +13,7 @@ int main()
         std::sregex_iterator()));
     std::cout << match_count << std::endl;
 
-    std::regex const doubleLetter("(.)\1");
-    if (std::regex_search(text, doubleLetter)) {
-        std::cout << "doubleLetter" << std::endl;
-    }
-
-    //if (std::regex_match ("bottle", std::regex("(.)\1") ))
-    if (std::regex_search("botle" ,std::regex("(.)\\1"))) {
+    if (std::regex_search("bottle" ,std::regex("(.)\\1"))) {
       std::cout << "string:literal => matched\n";
     }
     else {
